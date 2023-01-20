011bf4757f339e7d6852a03ab9cc7532b8df6d08
unknown <leonardoto.7.11@gmail.com>
2023-01-19 23:50:44 -0600

    final del curso

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,5 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
-import { devices } from '@playwright/test';
+// @ts-check
+const { devices } = require('@playwright/test');
 
 /**
  * Read environment variables from file.
@@ -7,11 +7,13 @@ import { devices } from '@playwright/test';
  */
 // require('dotenv').config();
 
+
 /**
- * See https://playwright.dev/docs/test-configuration.
+ * @see https://playwright.dev/docs/test-configuration
+ * @type {import('@playwright/test').PlaywrightTestConfig}
  */
-const config: PlaywrightTestConfig = {
-  testDir: './tests',
+const config = {
+  testDir: './e2e',
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   expect: {
@@ -36,7 +38,7 @@ const config: PlaywrightTestConfig = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://playwright.dev/docs/intro',
+    // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -104,4 +106,4 @@ const config: PlaywrightTestConfig = {
   // },
 };
 
-export default config;
+module.exports = config;
--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -1,34 +1,38 @@
 import { test, expect } from '@playwright/test';
-
+ 
 test.beforeEach(async ({ page }) => {
   await page.goto('/');
-});
+ });
 
 test('Realizar una busqueda que no tenga resultados', async ({ page }) => {
-  await page.getByRole('button').click();
-
-  await page.getByPlaceholder('Search docs').click();
+  
+  await page.getByRole('button', { name: 'Search' }).click();
+  await page.locator('internal:attr=[placeholder="Search docs"i]').click();
 
   await page.getByPlaceholder('Search docs').fill('hascontent');
 
-  expect(page.locator('.DocSearch-NoResults p')).toBeVisible();
+  await expect(page.locator('.DocSearch-Title')).toBeVisible();
 
-  expect(page.locator('.DocSearch-NoResults p')).toHaveText('No results for hascontent');
+  await expect(page.locator('.DocSearch-Title')).toHaveText('No results for "hascontent"');
 
 })
 
 test('Limpiar el input de busqueda', async ({ page }) => {
+
   await page.getByRole('button', { name: 'Search' }).click();
 
-  const searchBox = page.getByPlaceholder('Search docs');
+  const searchBox = page.locator('internal:attr=[placeholder="Search docs"i]');
 
   await searchBox.click();
 
   await searchBox.fill('somerandomtext');
 
-  await expect(searchBox).toHaveText('somerandomtext');
+  await expect(searchBox).toBeVisible();
 
-  await page.getByRole('button', { name: 'Clear the query' }).click();
+  await expect(searchBox).toHaveAttribute('value','somerandomtext');
+
+
+  await page.locator('.DocSearch-Reset').click();
 
   await expect(searchBox).toHaveAttribute('value', '');
 });
@@ -40,9 +44,10 @@ test('Realizar una busqueda que genere al menos tenga un resultado', async ({ pa
 
   await searchBox.click();
 
-  await page.getByPlaceholder('Search docs').fill('havetext');
+  await page.getByPlaceholder('Search docs').fill('toHaveText​');
 
-  expect(searchBox).toHaveText('havetext');
+  const expectedSearch = page.getByRole('link', { name: 'toHaveText​ LocatorAssertions' });
+  await expect(expectedSearch).toContainText('toHaveText​');
 
   // Verity there are sections in the results
   await page.locator('.DocSearch-Dropdown-Container section').nth(1).waitFor();
